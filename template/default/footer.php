c4ea2fee0bb74417b807106aeca0c8389ab2be6a
crx <crxane@mail.ru>
2026-01-11 20:35:26 -0500

    template work in prog

---
+++ template/default/footer.php
@@ -0,0 +1,14 @@
+</div>
+</div>
+<div id="footer">
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+	<td class="left"></td>
+	<td class="center"></td>
+	<td class="right"></td>
+  </tr>
+</table>
+</div>
+</div>
+</body>
+</html>
